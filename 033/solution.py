f8f922a08c9c0f8b44486d8f7cde1baa0dd2a0f0
andrew degirolamo <andrew@playorbits.com>
2025-08-14 12:13:43 -0700

    #33!

--- 033/solution.py
+++ 033/solution.py
@@ -2,8 +2,9 @@ import math
 
 numerators = []
 denominators = []
+
 for n in range (10,100):
-    for d in range(n +1, 100):
+    for d in range(n+1, 100):
         stringed_num = str(n)
         stringed_denom = str(d)
         if('0' in stringed_denom or '0' in stringed_num):
@@ -16,9 +17,6 @@ for n in range (10,100):
             if(int(stringed_num[0]) / int(stringed_denom[1]) == n / d):
                 numerators.append(n)
                 denominators.append(d)
-
-print(numerators)
-print(denominators)
 
 print(math.prod(denominators) // math.prod(numerators))
 
